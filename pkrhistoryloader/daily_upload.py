329245d8ab5041b07522c5007f9970c69f118162
Manggy94 <alex.mangwa@gmail.com>
2023-11-06 23:25:51 +0100

    Modif' de l'orga du loader

--- pkrhistoryloader/daily_upload.py
+++ pkrhistoryloader/daily_upload.py
@@ -4,4 +4,5 @@ from loader import S3Uploader
 if __name__ == '__main__':
     uploader = S3Uploader()
     year = datetime.date.today().year
-    uploader.upload_files(force_upload=True)
+    date = datetime.date(year, 8, 1)
+    uploader.upload_files_since(date=date, force_upload=True)
--- pkrhistoryloader/frequent_upload.py
+++ pkrhistoryloader/frequent_upload.py
@@ -1,6 +1,5 @@
 from loader import S3Uploader
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     uploader = S3Uploader()
     uploader.upload_today_files(force_upload=True)
